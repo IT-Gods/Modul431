7b02d2ce4827cb3b1942b92a9299a9dbf7cacb98
MorayYesilgueller <moray.yesilgueller@edu.tbz.ch>
2023-09-08 12:49:25 +0200

    created bullets for player

--- sourcecode/game_components.py
+++ sourcecode/game_components.py
@@ -1,18 +1,39 @@
 import pygame
 
-
-def makeRect(color,XY,SizeWH,screen):
-    playerRect = pygame.Rect(XY[0],XY[1],Size[0],Size[1])
-    pygame.draw.rect(screen,color,playerRect)
+#collection of functions used in the game
 
 
 
-def updatePlayerXY(playerXY, playerSize,displaySize):
-    pressed= pygame.key.get_pressed()
+#create a rectangle is used as filler for models
+def makeRect(color,XY,sizeWH,screen):
+    myRect = pygame.Rect(XY[0],XY[1],sizeWH[0],sizeWH[1])
+    pygame.draw.rect(screen,color,myRect)
+
+
+#function to update player movement
+def updatePlayerXY(playerXY, playerSize,pressed,displaySize):
     if pressed[pygame.K_LEFT] and playerXY[0] != 0:
         playerXY[0] -= 0.5
     if pressed[pygame.K_RIGHT] and playerXY[0] + playerSize[1] != displaySize[0] :
         playerXY[0] += 0.5
+    return playerXY
 
+
+#function to update fire and detect collision returns new coordinates of bullet
+def updateFire(color,XY,sizeWH,screen):
+    XY[1] -=1
+    makeRect(color,XY,sizeWH,screen)
+
+    return XY
+
+
+def detectCollision(XY,sizeWH,screenSize):
+
+    if XY[0] == 0 or XY[0] == screenSize[0]:
+        return False
+    elif XY[1] == 0 or XY[1] == screenSize[1]:
+        return False
+    else:
+        return True
 
 
